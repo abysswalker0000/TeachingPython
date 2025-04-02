cc2921733a9d68e60c7092e620bd4986d6c15917
abysswalker0000 <abysswalker0000@mail.ru>
2025-04-03 00:56:14 +0300

    small fixes
    new game field + better design to implement score , timer and restart button

--- Games/snake/game.py
+++ Games/snake/game.py
@@ -1,19 +1,18 @@
 import pygame
 import random
-from settings import * 
+from settings import *
 
-
-class SnakeGame: 
+class SnakeGame:
     def __init__(self):
-        self.snake = [(WINDOW_WIDTH // 2 , WINDOW_HEIGHT // 2 )]
+        self.snake = [(FIELD_WIDTH // 2, FIELD_HEIGHT // 2)]
         self.direction = (GRID_SIZE, 0)
         self.food = self.spawn_food()
         self.game_over = False
 
     def spawn_food(self):
-        x = random.randrange(0, WINDOW_WIDTH, GRID_SIZE)
-        y = random.randrange(0, WINDOW_HEIGHT, GRID_SIZE)
-        return(x,y)
+        x = random.randrange(0, FIELD_WIDTH, GRID_SIZE)
+        y = random.randrange(0, FIELD_HEIGHT, GRID_SIZE)
+        return (x, y)
     
     def update(self):
         if self.game_over:
@@ -25,29 +24,27 @@ class SnakeGame:
             self.direction = (0, GRID_SIZE)
         if keys[pygame.K_LEFT] and self.direction != (GRID_SIZE, 0):
             self.direction = (-GRID_SIZE, 0)
-        if keys[pygame.K_RIGHT] and self.direction != (-GRID_SIZE,0):
+        if keys[pygame.K_RIGHT] and self.direction != (-GRID_SIZE, 0):
             self.direction = (GRID_SIZE, 0)
         
         head_x, head_y = self.snake[0]
         new_head = (head_x + self.direction[0], head_y + self.direction[1])
-        self.snake.insert(0,new_head)
+        self.snake.insert(0, new_head)
 
         if new_head == self.food:
-            self.food =self.spawn_food()
-        else :
+            self.food = self.spawn_food()
+        else:
             self.snake.pop()
         
-        if (new_head[0] < 0 or new_head[0] >= WINDOW_WIDTH or
-            new_head[1] < 0 or new_head[1] >= WINDOW_HEIGHT):
+        if (new_head[0] < 0 or new_head[0] >= FIELD_WIDTH or
+            new_head[1] < 0 or new_head[1] >= FIELD_HEIGHT):
             self.game_over = True
 
         if new_head in self.snake[1:]:
             self.game_over = True
 
-    def draw(self, screen):
-        screen.fill(BLACK)
-
+    def draw(self, field):
+        field.fill(BLACK)
         for segment in self.snake:
-            pygame.draw.rect(screen, GREEN, (segment[0], segment[1], GRID_SIZE, GRID_SIZE))
-
-        pygame.draw.rect(screen, RED, (self.food[0], self.food[1], GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(field, GREEN, (segment[0], segment[1], GRID_SIZE, GRID_SIZE))
+        pygame.draw.rect(field, RED, (self.food[0], self.food[1], GRID_SIZE, GRID_SIZE))
--- Games/snake/main.py
+++ Games/snake/main.py
@@ -5,20 +5,25 @@ from settings import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    field = pygame.Surface((FIELD_WIDTH, FIELD_HEIGHT))
     pygame.display.set_caption("Snake Game")
     clock = pygame.time.Clock()
-
     game = SnakeGame()
-
+    
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running=False
+                running = False
+        
         game.update()
-        game.draw(screen)
+        screen.fill(BLACK)
+        pygame.draw.rect(screen, WHITE, (95, 95, FIELD_WIDTH + 10, FIELD_HEIGHT + 10), 5)
+        game.draw(field)
+        screen.blit(field, (100, 100))
         pygame.display.flip()
         clock.tick(FPS)
+    
     pygame.quit()
 
 if __name__ == "__main__":
